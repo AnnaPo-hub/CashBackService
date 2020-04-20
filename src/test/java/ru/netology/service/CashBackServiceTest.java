616151534037da690ba70ed3a538432182c9f536
Anna Popova <anna.popova201909@gmail.com>
2020-04-20 16:32:22 +0300

    added one more test

--- src/test/java/ru/netology/service/CashBackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackServiceTest.java
@@ -5,10 +5,16 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 class CashBackServiceTest {
+    CashBackService cashBackService = new CashBackService();
 
    @Test
-    public void remain() {
-        CashBackService cashBackService = new CashBackService();
+    public void shouldCheckRemain() {
         assertEquals (cashBackService.remain(900), 100);
     }
+
+    @Test
+    public void shouldCheckRemainIfAmountEqualToBoundary() {
+            assertEquals(cashBackService.remain(1000), 0 );
+
+        }
 }

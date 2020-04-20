3e48d8571319a54db2aa051e2eb6339431b6f70e
Anna Popova <anna.popova201909@gmail.com>
2020-04-20 16:27:39 +0300

    added one more test

--- src/test/java/ru/netology/service/CashBackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackServiceTest.java
@@ -7,8 +7,17 @@ import static org.junit.Assert.*;
 public class CashBackServiceTest {
 
     @org.junit.Test
-    public void remain() {
+    public void showdCheckRemain() {
         CashBackService cashBackService = new CashBackService();
         assertEquals(100,cashBackService.remain(900) );
+
+    }
+
+    @org.junit.Test
+    public void showdCheckRemainIfAmountIsEqualToBoundary() {
+        CashBackService cashBackService = new CashBackService();
+        assertEquals(0,cashBackService.remain(1000) );
+
+
     }
 }

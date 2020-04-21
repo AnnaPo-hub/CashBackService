062eab1455951e9ed7bd4139df8a89de9f1e5b9a
Anna Popova <anna.popova201909@gmail.com>
2020-04-21 21:59:39 +0300

    edited according to the comments

--- src/test/java/ru/netology/service/CashBackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackServiceTest.java
@@ -1,30 +1,25 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class CashBackServiceTest {
+    CashBackService cashBackService = new CashBackService();
 
     @org.junit.Test
     public void shouldReturn100IfAmountIs900() {
-        CashBackService cashBackService = new CashBackService();
-        assertEquals(100,cashBackService.remain(900) );
-
+        int actual = cashBackService.remain(900);
+        assertEquals(100, actual);
     }
 
     @org.junit.Test
     public void shouldReturn1000IfAmountIsEqualToBoundary() {
-        CashBackService cashBackService = new CashBackService();
-        assertEquals(1000,cashBackService.remain(1000) );
-
-
+        int actual = cashBackService.remain(1000);
+        assertEquals(1000, actual);
     }
-
 
     @org.junit.jupiter.api.Test
     void shouldReturn100ifAmountIs900() {
-        CashBackService cashBackService = new CashBackService();
-        assertEquals(100, cashBackService.remain(900));
+        int actual = cashBackService.remain(900);
+        assertEquals(100, actual);
     }
 }
